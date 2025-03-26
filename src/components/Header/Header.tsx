86eb96a9aeff0c247e4f9cd8d3f5e1be5228fc8f
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2025-03-26 12:14:46 +0100

    Add CSS Modules styling for Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
+import styles from "./Header.module.css";
+
 interface HeaderProps {
   title: string;
 }
 
 export default function Header({ title }: HeaderProps) {
   return (
-    <header>
+    <header className={styles.header}>
       <h1>{title}</h1>
     </header>
   );
